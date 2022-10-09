83fb4c183884cb1074c4013c489a496f4764ad5c
anil843324 <anilkrsingh14200@gmail.com>
2022-10-09 10:38:06 +0530

    connect with atlas

--- db/config.js
+++ db/config.js
@@ -1,5 +1,13 @@
 const mongoose=require('mongoose');
 
- mongoose.connect("mongodb://localhost:27017/e-commerce")
-
  
+//  const mongoose = require('mongoose');
+// const connectionString = process.env.DATABASE_URL;
+
+
+
+// await mongoose.connect(connectionString);
+// mongodb+srv://root:<password>@cluster0.sj56kau.mongodb.net/test
+
+ mongoose.connect("mongodb+srv://root:root@cluster0.sj56kau.mongodb.net/test")
+
